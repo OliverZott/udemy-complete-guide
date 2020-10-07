9b98b0ce8d93c43312e8c45427f858b8321f1183
OZ <developers.team+oz@gotoemma.de>
2020-10-07 18:54:34 +0200

    Implements Sophisticated version (udemy) "Custom-Attribute-Directive" for opening dropdown (bootstrap class "open")

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
@@ -14,9 +14,15 @@ export class DropdownDirective {
     /**
      * Directive "appDropdown" sits on html-element and on click adds css-class "open" (bootstrap)
      */
-    @HostListener('click') toggleOpen(): void {
+    @HostListener('blub') toggleOpenSimple(): void {       // change 'blub' to 'click' to work!
         this.isOpen = !this.isOpen;
 
     }
 
+    @HostListener('document:click', ['$event']) toggleOpenSoph(event: Event): void {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    }
+
+    constructor(private elRef: ElementRef) {
+    }
 }
