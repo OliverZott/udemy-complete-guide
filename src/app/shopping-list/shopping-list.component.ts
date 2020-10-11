6e297bc3d31a95fd5348f392a676a13ee6f3e901
OliverZott <zott_oliver@web.de>
2020-10-11 10:08:48 +0200

    Implements shopping-list service for adding ingredients; ToDo: emitter for edit-list functionality

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css'],
+    providers: [ShoppingListService]
 })
 export class ShoppingListComponent implements OnInit {
 
-    ingredients: Ingredient[] = [];
+    ingredients: Ingredient[];
 
-  constructor() { }
+    constructor(private shoppingListService: ShoppingListService) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.ingredients = this.shoppingListService.getIngredients();
+    }
 
     onIngredientAdded(ingredient: Ingredient): void {
-        this.ingredients.push(ingredient);
-        console.log(this.ingredients);
+        this.shoppingListService.addIngredient(ingredient);
     }
+
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,20 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
 export class ShoppingListService {
-    
+    // should manage shipping list / list ofingredients
+
+    private ingredients: Ingredient[] = [];
+
+    public ingredientAdded: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+
+    public getIngredients(): Ingredient[] {
+        return this.ingredients;
+    }
+
+    public addIngredient(ingredient: Ingredient): void {
+        this.ingredients.push(ingredient);
+        console.log(this.ingredients);
+    }
+
 }
